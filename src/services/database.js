28c8dbdcec0200d210b0ed98db63f8638a717ea2
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-04-12 20:47:19 -0300

    pegar produtos

--- src/services/database.js
+++ src/services/database.js
@@ -1,12 +1,25 @@
-import db from '../index'
+import db from '../index';
 
 const getProducts = async () => {
   try {
     const result = await db.collection('products').get();
-    console.log(result);
+    // console.log(
+    //   result.docs.forEach((doc) => {
+    //     console.log(
+    //       doc.data().processes.forEach((process) => {
+    //         console.log(process);
+    //       })
+    //     );
+    //   })
+    // );
+    console.log(
+      result.docs.forEach((doc) => {
+        console.log(doc.data().name);
+      })
+    );
   } catch (error) {
     console.log(error);
   }
 };
 
-export default getProducts
+export default getProducts;
