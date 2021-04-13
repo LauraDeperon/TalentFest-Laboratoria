c0324b2adae07b1e0823c6ef56653bff2e40ac30
Laura Deperon Ribeiro <70659755+LauraDeperon@users.noreply.github.com>
2021-04-13 18:37:36 -0300

    Feature/componente informações produtos (#5)
    * informações dos produtos
    
    * produtos informações

---
+++ src/components/productsInformation/productsInformation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './productsInformation.css';
+
+function productsInformation({ item }) {
+  return (
+    <>
+      <div className='App'>
+        <p>{item.name}</p>
+        <p>{item.brand}</p>
+        <p>{item.description}</p>
+        {item.processes.map((process) => console.log(process))}
+        <p>{item.recipe}</p>
+      </div>
+    </>
+  );
+}
+
+export default productsInformation;
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,67 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import getProducts from "../services/database"
+import getProducts from '../services/database';
 import './products.css';
-import Category from '../components/category'
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Category from '../components/category';
+import Footer from '../components/footer';
+import Header from '../components/header';
+import productsInformation from '../components/productsInformation/productsInformation'
 
 function Products() {
-
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    
-    getProducts().then((result) => {
-      result.docs.forEach((doc) => {
-         setProducts(obj => [...obj, doc.data()]);
-            })
-      
-    })
-    console.log(products)
-  }, [])
-
-console.log(products)
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <button onClick={() => getProducts()}></button>
-      </header>
-      <Header/>
-      <Category/>
-      <Footer/>
-    </div>
-  );
-
-  useEffect(() => {
-    if (products.length === 0){
+    if (products.length === 0) {
       getProducts().then((result) => {
         result.docs.forEach((doc) => {
-          setProducts(obj => [...obj, doc.data()]);
-        })
-      })
+          setProducts((obj) => [...obj, doc.data()]);
+        });
+      });
     }
-  }, [products])
+  }, [products]);
 
   return (
     <>
       <Header />
       <Category />
       <div className='App'>
-        <h1>Olá</h1>
-        {products.map((item) => {
+        {products.map((item, index) => {
           return (
-            <div key={item.id}>
+            <div key={index}>
               {/* <p>{item.image}</p> */}
-              <p>{item.name}</p>
-              {/* <button onClick={() => ProductsInformation item={item}}>ver</button> */}
+              <p>{item.name + ' ' + item.brand}</p>
+              <button onClick={() => productsInformation((item = { item }))}>
+                ver
+              </button>
             </div>
-          )
+          );
         })}
         <Footer />
       </div>
     </>
-  )
-
+  );
 }
 
 export default Products;
