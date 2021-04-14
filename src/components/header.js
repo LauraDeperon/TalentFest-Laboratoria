e29d548c02cee60663c22f9a10778bbec70536bc
caroline costa <carolmarcionilia@gmail.com>
2021-04-14 00:45:47 -0300

    add new close img

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,7 @@ function Header() {
           <button className='btn-about' onClick={() => setModalIsOpen(true)}>Sobre</button>
           <Modal className='modal' isOpen={modalIsOpen}
             onRequestClose={closeModalSobre}>
-            <button className='btn-close' onClick={closeModalSobre}><img src={Close} alt='' /></button>
+            <button className='btn-close' onClick={closeModalSobre}><img className='img-close' src={Close} alt='' /></button>
             <p>A Cargill oferece serviços e produtos alimentícios, agrícolas, financeiros e industriais ao mundo.
             Juntamente com produtores rurais, clientes, governos e comunidades, ajudamos as pessoas a prosperar com a aplicação de nossas ideias há mais de 155 anos. Temos 155.000 funcionários em 70 países, que estão comprometidos a fornecer alimentos ao mundo de uma forma responsável, reduzindo o impacto ambiental e melhorando as comunidades onde atuamos.
             </p>
