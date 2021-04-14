20beaf53dba731a7de0b18fac0160a7f281c86fc
caroline costa <carolmarcionilia@gmail.com>
2021-04-14 00:17:42 -0300

    add basic modal style

--- src/components/productsInformation/productsInformation.js
+++ src/components/productsInformation/productsInformation.js
@@ -3,12 +3,12 @@ import React from 'react';
 function productsInformation({ item }) {
   return (
     <>
-      <div className='App'>
-        <p>{item.name}</p>
-        <p>{item.brand}</p>
-        <p>{item.description}</p>
+      <div className='products-section'>
+        <p className='product-type'>{item.name}</p>
+        <p className='product-type'>{item.brand}</p>
+        <p className='product-type'>{item.description}</p>
         {item.processes.map((process) => console.log(process))}
-        <p>{item.recipe}</p>
+        <p className='product-type'>{item.recipe}</p>
       </div>
     </>
   );
