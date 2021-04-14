3f2ddd160193d7ec39d0b86831da39977640d2bb
caroline costa <carolmarcionilia@gmail.com>
2021-04-14 08:28:11 -0300

    fix content structure

--- src/components/category.js
+++ src/components/category.js
@@ -2,24 +2,27 @@ import Chocolate from '../imagens/chocolate.png'
 import Atomatado from '../imagens/tomate.png'
 import Oleos from '../imagens/oleo.png'
 function Category() {
-    return (
-      <div className='Category'>
-          <nav>
-              <span>
-              <img src={Atomatado} alt='' width="100" />
-                  <p>Atomatados</p>
-                </span>
-                <span>
-                <img src={Chocolate} alt='' width="100" />
-                  <p>Chocolates</p>
-                </span>
-                <span>
-                <img src={Oleos} alt='' width="100" />
-                  <p>Óleos</p>
-                </span>
-          </nav>
-      </div>
-    );
-  }
-  
-  export default Category;
+  return (
+    <>
+      <header>
+        <p className='category-title'>Categorias</p>
+      </header>
+      <nav className='category'>
+        <div className='category-info'>
+          <img className='img-product-category' src={Atomatado} alt='' />
+          <p className='product-category'>Atomatados</p>
+        </div>
+        <div className='category-info'>
+          <img className='img-product-category' src={Chocolate} alt='' />
+          <p className='product-category'>Chocolates</p>
+        </div>
+        <div className='category-info'>
+          <img className='img-product-category' src={Oleos} alt='' />
+          <p className='product-category'>Óleos de cozinha</p>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Category;
